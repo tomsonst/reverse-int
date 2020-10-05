2be0611cf08285fc9223f230ad7c461e94f7fe66
tomsonst <40933547+tomsonst@users.noreply.github.com>
2020-10-05 21:15:26 +0300

    feat: add solution reverse int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(num){
+    let str = String(Math.abs(num));
+    num = Number(str.split("").reverse().join(""));
+    return(num);
+   }
